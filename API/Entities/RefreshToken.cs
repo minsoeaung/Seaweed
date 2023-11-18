315d18acbe4912fac58da67b5127223cdeddbb81
minsoeaung <minsoeaung2001@gmail.com>
2023-11-18 16:10:09 +0630

    Refresh token data time is nullable in user table

--- API/Entities/RefreshToken.cs
+++ API/Entities/RefreshToken.cs
@@ -6,6 +6,6 @@ namespace API.Entities;
 public class RefreshToken
 {
     public string? Token { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ExpiredAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? ExpiredAt { get; set; }
 }
